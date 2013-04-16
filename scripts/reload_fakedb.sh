39aae956a743c3ac92a98f30651752917af952a5
Robert Helmer <rhelmer@rhelmer.org>
2013-04-16 14:25:34 -0700

    cleanup debugging

--- scripts/reload_fakedb.sh
+++ scripts/reload_fakedb.sh
@@ -8,13 +8,10 @@ export PGPORT=$3
 export PGHOST=$4
 
 #  create empty DB named "fakedata"
-#dropdb fakedata
-#createdb fakedata
 ./socorro/external/postgresql/setupdb_app.py --database_name=fakedata --database_password=fakedata --dropdb --force
-#psql -f sql/schema.sql fakedata > schema.log
+
 # generate fakedata
 ./socorro/external/postgresql/fakedata.py > load.sql
 
 # load fakedata into DB
 psql fakedata < load.sql > load.out
-
