19fc7c658ec8309c268d6eb624baf34790eb0ea5
Robert Helmer <rhelmer@rhelmer.org>
2013-06-12 11:11:32 -0700

    bug 871056 - clean up dependencies

--- puppet/manifests/nodes/nodes.pp
+++ puppet/manifests/nodes/nodes.pp
@@ -2,12 +2,11 @@ node default {
     include socorro-base
     include socorro-python
     include socorro-web
-    include socorro-java
     include socorro-db
     include socorro-monitor
     include socorro-processor
     include socorro-collector
     include socorro-api
+#    include socorro-java
 #    include socorro-hbase
-#    include socorro-elasticsearch
 }
