aa6a361c8dab79e66670da2964266b8211f0cc79
Robert Helmer <rhelmer@rhelmer.org>
2015-01-08 08:18:23 -0800

    add deps to setup.py for socorro-lite

--- setup.py
+++ setup.py
@@ -12,18 +12,11 @@ except ImportError:
     pass
 
 
-def read(fname):
-    fpath = os.path.join(os.path.dirname(__file__), fname)
-    with codecs.open(fpath, 'r', 'utf8') as f:
-        return f.read().strip()
-
-
 setup(
     name='socorro',
     version='master',
     description=('Socorro is a server to accept and process Breakpad'
                  ' crash reports.'),
-    long_description=open('README.md').read(),
     author='Mozilla',
     author_email='socorro-dev@mozilla.com',
     license='MPL',
@@ -39,7 +32,17 @@ setup(
     keywords=['socorro', 'breakpad', 'crash', 'reporting', 'minidump',
               'stacktrace'],
     packages=find_packages(),
-    install_requires=[], # use peep -r requirements.txt instead
+    install_requires=['configman==1.2.8', 'configobj==4.7.2',
+        'isodate==0.4.7', 'lxml==2.3.4', 'pika==0.9.8',
+        'elasticsearch==1.2', 'elasticsearch-dsl==0.0.2',
+        'pyelasticsearch==0.6.1', 'urllib3==1.9.1', 'elasticutils==0.7',
+        'raven==3.4.1', 'requests==1.2.3', 'simplejson==2.5.0', 'six==1.7.3',
+        'statsd==2.1.2', 'suds==0.4', 'web.py==0.36',
+        'wsgiref==0.1.2', 'ujson==1.33', 'python-dateutil==2.1',
+        'ordereddict==1.1', 'crontabber==0.15', 'boto==2.28.0',
+        'pyquery==1.2.6', 'python-memcached==1.48', 'BeautifulSoup==3.2.1',
+        'path.py==5.1', 'sasl==0.1.3', 'pyOpenSSL==0.14',
+        'ndg-httpsclient==0.3.2', 'pyasn1==0.1.7', 'poster==0.8.1'], 
     entry_points={
         'console_scripts': [
                 'socorro = socorro.app.socorro_app:SocorroWelcomeApp.run'
